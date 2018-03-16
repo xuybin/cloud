5800b824d665b16cf926fd8b033c981630a04313
YongBin Xu <xuybin@qq.com>
2018-03-16 16:47:35 +0800

    整理

---
+++ zuul/src/main/java/people/data/cloud/zuul/TokenFilter.java
@@ -0,0 +1,54 @@
+package people.data.cloud.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class TokenFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(TokenFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        //可选择不过滤静态资源
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        String accessToken = request.getHeader("Authorization");
+        if(StringUtils.isAllBlank(accessToken) && StringUtils.equalsIgnoreCase(request.getMethod(),"GET")){
+            accessToken = request.getParameter("token");
+        }
+
+        if(StringUtils.isAllBlank(accessToken)) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (IOException e){}
+            return null;
+        }
+        return null;
+    }
+}
