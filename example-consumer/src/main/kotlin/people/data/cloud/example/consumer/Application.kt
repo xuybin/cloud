13ee55acfce5f3a6e5fc5696cdb9ff9e3d0402ee
YongBin Xu <xuybin@qq.com>
2018-03-16 18:11:29 +0800

    整理

--- example-consumer/src/main/kotlin/people/data/cloud/example/consumer/Application.kt
+++ example-consumer/src/main/kotlin/people/data/cloud/example/consumer/Application.kt
@@ -2,10 +2,16 @@ package people.data.cloud.example.consumer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 class ExampleConsumerApplication
 
 fun main(args: Array<String>) {
--- example-publisher/src/main/kotlin/people/data/cloud/example/publisher/Application.kt
+++ example-publisher/src/main/kotlin/people/data/cloud/example/publisher/Application.kt
@@ -3,9 +3,13 @@ package people.data.cloud.example.publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 class ExamplePublisherApplication
 
 fun main(args: Array<String>) {
